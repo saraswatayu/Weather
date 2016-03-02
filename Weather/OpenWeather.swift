696199724281a13a755ee49bd1487e63c02eebd6
Ayush Saraswat <saraswatayu@gmail.com>
2016-03-02 12:39:17 -0800

    remove carthage + reactivecocoa

--- Weather/OpenWeather.swift
+++ Weather/OpenWeather.swift
@@ -7,7 +7,6 @@
 //
 
 import Foundation
-import ReactiveCocoa
 
 final class OpenWeather {
     
--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import ReactiveCocoa
 
 class ViewController: UIViewController {
 
